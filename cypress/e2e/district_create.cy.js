60710218d390394fc1a14da209eb1e809118d115
Ekaterina Flowruns <flowruns@yandex.ru>
2025-03-24 16:09:32 +0200

    cypressTest

---
+++ cypress/e2e/district_create.cy.js
@@ -0,0 +1,48 @@
+﻿describe('Реестр адресов. Создание района', () => {
+    it('Создание района', () => {
+        cy.fixture('districtData').then(data => {
+            
+            // Авторизируемся
+            cy.userAuthorization();
+            
+            cy.log('Открываем реестр адресов');
+
+            // Нажимаем на кнопку в сайдбаре "Адресный фонд"
+            cy.get('[data-test-id="Адресный фонд"]').click();
+
+            // Проверяем URL
+            cy.url().should('eq', 'https://demo.app.stack-it.ru/fl/accounts/acc_menu');
+
+            // Нажимаем на кнопку "Адреса проживающих"
+            cy.get('[data-test-id="Адреса проживающих"]').click();
+
+            // Проверяем URL
+            cy.url().should('eq', 'https://demo.app.stack-it.ru/fl/accounts');
+
+            cy.log('Создаем район');
+
+            // Нажимаем на кнопку в навбаре реестра "+"
+            cy.get('[data-cy="btn-add"]').click();
+
+            // Нажимаем на пункт меню "Район"
+            cy.get('div.menuable__content__active :first-child[data-cy="stack-menu-list-item"]').click();
+
+            // Ждем открытие модалки создания района
+            cy.wait(2000);
+            
+            // Проверяем, что окрылось модальное окно создания района
+            cy.get('.v-dialog').should('be.visible');
+
+            // Заполняем поле в модальном окне "Название района"
+            cy.get('[data-test-id="Название района"]').type(data.districtName, {timeout: 3000});
+
+            // Нажимаем на кнопку в модальном окне "Внести"
+            cy.get('.v-dialog [data-cy="btn-save"]').click();
+
+            cy.log('Проверка созданного района в реестре');
+            
+            // Проверяем, что реестр отобразился и район создался
+            cy.rowsCountBeforeDelete();
+        })
+    });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,51 @@
+﻿/**
+ * Кастомная команда для авторизации пользователя
+ */
+Cypress.Commands.add('userAuthorization', () => {
+    
+    // Используем фикстуру с данными логина
+    cy.fixture('loginData').then(data => {
+
+        // Переходим на целевой url
+        cy.visit(data.url)
+
+        // Заполняем поле "Логин"
+        cy.get('input#VInput71').type(data.login, {timeout: 5000});
+
+        // Заполняем поле "Пароль"
+        cy.get('input#VInput75').type(data.password);
+
+        // Нажимаем на кнопку "Войти"
+        cy.get('button[data-cy="submit-btn"]').click();
+
+        // Ждем авторизацию
+        cy.wait(2000);
+
+        // Проверяем URL
+        cy.url().should('eq', data.url);
+    })
+})
+
+/**
+ * Проверка кол-ва строк реестра после создания записи 
+ */
+Cypress.Commands.add('rowsCountBeforeDelete', () => {
+    
+    // Проверяем, что таблица реестра есть
+    cy.get('.v-data-table__wrapper table').should('be.visible');
+
+    // Проверяем,
+    cy.get('.v-data-table__wrapper table').find('tr').should('have.length', 5);
+})
+
+/**
+ * Ожидание, что кол-во строк в реестер после удаления будет равно - 4
+ */
+Cypress.Commands.add('rowsCountAfterDelete', () => {
+
+    // Проверяем, что таблица реестра есть
+    cy.get('.v-data-table__wrapper table').should('be.visible');
+
+    // Проверяем,
+    cy.get('.v-data-table__wrapper table').find('tr').should('have.length', 4);
+})
